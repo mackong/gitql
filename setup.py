f68ac8f10ecda3d91973cb22c828e2b9f351558d
mackong <mackonghp@gmail.com>
2017-01-07 12:00:12 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+import os
+
+import gitql
+
+
+def extra_dependencies():
+    import sys
+    ret = []
+    if sys.version_info < (2, 7):
+        ret.append('argparse')
+    return ret
+
+
+def read(*names):
+    values = dict()
+    extensions = ['txt', 'md']
+    for name in names:
+        value = ''
+        for ext in extensions:
+            filename = name + ext
+            if os.path.isfile(filename):
+                value = open(filename).read()
+                break
+        values[name] = value
+    return values
+
+
+long_description = """
+%(README)s
+""" % read('README')
+
+setup(
+    name='gitql',
+    version=gitql.__version__,
+    description='A git query language',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Indented Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+    ],
+    keywords='git gitql',
+    author='MacKong',
+    author_email='mackonghp@gmail.com',
+    maintainer='MacKong',
+    maintainer_email='mackonghp@gmail.com',
+    url='https://github.com/mackong/gitql',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['gitql = gitql.main:main', ]},
+    install_requires=[
+        'GitPython',
+        'prettytable',
+        'termcolor',
+    ] + extra_dependencies())
